03f088aae975f5f34c8007ec16e80cf269800f4a
Khaled ElMorshedy <31712173+elmoiv@users.noreply.github.com>
2019-05-07 00:17:31 +0200

    Update sdelmo.py

--- sdelmo.py
+++ sdelmo.py
@@ -1,3 +1,20 @@
+#       sdelmo.py, simple Souncloud music downloader.
+#
+#       Copyright 2019 Khaled El-Morshedy <elmoiv>
+#
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the attached License.
+#
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+
 import requests, json, urllib.request, os
 from lxml import html
 from tqdm import tqdm
