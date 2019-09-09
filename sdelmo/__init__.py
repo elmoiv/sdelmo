c65c39a7a1e3722868bb43863912e50af3324310
Khaled ElMorshedy <31712173+elmoiv@users.noreply.github.com>
2019-09-09 18:46:01 +0200

    Add files via upload

---
+++ sdelmo/__init__.py
@@ -0,0 +1,9 @@
+from sdelmo.sdelmo import scdl
+
+"""A library for downloading mp3 files from SoundCloud"""
+
+__author__ = 'Khaled H. El-Morshedy'
+__url__ = 'https://github.com/elmoiv/sdelmo'
+__description__ = 'A simple soundcloud downloader'
+__license__ = 'MIT'
+__version__ = '2.0.2'
